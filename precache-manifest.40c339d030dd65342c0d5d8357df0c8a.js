0b711d0a72dea20c2767af865c86b47d222e071d
John Cambi <jcambi@myseneca.ca>
2019-06-26 10:22:57 -0400

    Updates

---
+++ precache-manifest.40c339d030dd65342c0d5d8357df0c8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1b1940d7747d79597ec8f9ab025226",
+    "url": "/react-firebase-authentication/index.html"
+  },
+  {
+    "revision": "fe65145741b943106c00",
+    "url": "/react-firebase-authentication/static/css/main.44b1b850.chunk.css"
+  },
+  {
+    "revision": "993d2ace6b1b1ea1e55d",
+    "url": "/react-firebase-authentication/static/js/2.60cc09e4.chunk.js"
+  },
+  {
+    "revision": "fe65145741b943106c00",
+    "url": "/react-firebase-authentication/static/js/main.620be219.chunk.js"
+  },
+  {
+    "revision": "0053efa8a6f30d19dfd4",
+    "url": "/react-firebase-authentication/static/js/runtime~main.ad14ce0f.js"
+  }
+]);
